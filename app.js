99e8847b4fa56a93f7c0845996185ee4d4d2a9c9
SANDEEPS115 <53892298+SANDEEPS115@users.noreply.github.com>
2025-04-13 08:44:25 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Sandeep Nair</h1>
+      <p>Software Tester | MCA Graduate</p>
+    </div>
+  );
+}
+
+export default App;
